73affd453032a4e82cecc4e3f796b525a73954a7
Alexander Barbara <aybarbara@gmail.com>
2017-10-09 11:46:56 -0400

    strip string fields

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  before_validation :strip_text_fields
+
+  def strip_text_fields
+    self.attributes.each do |field, val|
+      self.send(field).strip! if val.respond_to?(:strip)
+    end
+  end
 end
