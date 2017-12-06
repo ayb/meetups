b5db7e8a3ed4214d50b77ccb60b1b01eabed8854
Alexander Barbara <aybarbara@gmail.com>
2017-12-06 10:17:36 -0500

    filter on has many to get the :through we want

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,6 +3,15 @@ class Group < ApplicationRecord
   has_many :members, class_name: "User", through: :memberships
   has_many :roles, through: :memberships
 
+  # filter on the has_many to get the :through result we want (i.e. organizers)
+  has_many :organizer_memberships, -> { joins(:role).where("roles.name = ?", 'Organizer') },
+    class_name: "Membership"
+  has_many :organizers, through: :organizer_memberships, source: :member do
+    def names
+      self.map(&:name)
+    end
+  end
+
   validates :name, presence: true, uniqueness: true
 
   def organizer_names
